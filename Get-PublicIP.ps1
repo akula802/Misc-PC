342889d7364b938c5ac654b7a5d20d606123689f
Hartley <bhartley@tristategt.org>
2023-04-19 08:54:20 -0600

    Added -UseBasicParsing to command

--- Get-PublicIP.ps1
+++ Get-PublicIP.ps1
@@ -1,5 +1,5 @@
 # Just a one-liner I've been using lately, saving here for future reference
 
-$publicIP4 = (Invoke-WebRequest -uri "https://api.ipify.org/").Content
+$publicIP4 = (Invoke-WebRequest -uri "https://api.ipify.org/" -UseBasicParsing).Content
 Write-Host $publicIP4
 
